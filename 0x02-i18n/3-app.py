c37e755ec2ed4c2cd9bfb494a565b04f0fca1ff1
Ebenezer terabo <teraboebenezer@gmail.occom>
2024-09-14 00:46:49 +0000

    Task 4 solved

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -3,14 +3,15 @@
 from flask import Flask, render_template
 from babel_flask import Babel
 
+Config = __import__('0-app.py').Config
 
 # Create an instance of Flask and Babel
 app = Flask(__name__)
 babel = Babel(app)
 
-
+app.config.from_object(Config)
 # Create a single route
-@app.route('/')
+@app.route('/', methods=['GET'])
 def index():
     """ return index page """
     return render_template('3-index.html')
